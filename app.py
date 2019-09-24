247a1a80d00f5e12db57d05b3ff3d1ef8a3a66ba
pdusari <praveen.dusari@gmail.com>
2019-09-24 11:38:10 +0530

    m

--- app.py
+++ app.py
@@ -24,10 +24,5 @@ if __name__ == "__main__":
 	# starting thread 2 
 	t2.start() 
 
-	# wait until thread 1 is completely executed 
-	t1.join() 
-	# wait until thread 2 is completely executed 
-	t2.join() 
-
 	# both threads completely executed 
 	print("Done!") 
